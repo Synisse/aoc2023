6be908159f031317b590b3d16815979f4d97bf16
Stefan Krüger <stefan-krueger@hotmail.de>
2023-12-08 22:33:10 +0100

    solve day 8 part 2

--- src/day8/day8.ts
+++ src/day8/day8.ts
@@ -1,5 +1,5 @@
-import {DAY8_DATA, DAY8_DATA_DUMMY_A, DAY8_DATA_DUMMY_B} from './data';
-import {find} from 'lodash';
+import {DAY8_DATA, DAY8_DATA_DUMMY_A, DAY8_DATA_DUMMY_B, DAY8_DATA_DUMMY_C} from './data';
+import {endsWith, every, find, some} from 'lodash';
 
 interface Network {
 	directions: string;
@@ -21,9 +21,7 @@ function parseData(aData: string): Network {
 	const directions = data[0];
 
 	const networkEntries = data[1].split('\n').map((aNetworkEntry) => {
-		console.log('aNetworkEntry: ' + aNetworkEntry);
-
-		const networkValues = aNetworkEntry.match(/([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)/);
+		const networkValues = aNetworkEntry.match(/^(\w+) = \((\w+), (\w+)\)$/);
 		return {
 			origin: networkValues![1],
 			left: networkValues![2],
@@ -37,20 +35,54 @@ function parseData(aData: string): Network {
 	};
 }
 
-export function solveDay8() {
-	let network = parseData(DAY8_DATA) as Network;
+function solvePart1(aNetwork: Network) {
 	const destination = 'ZZZ';
 	let currentLocation = 'AAA';
 
 	let directionCounter = 0;
 
 	while (currentLocation !== destination) {
-		const direction = network.directions[directionCounter % network.directions.length];
-		const networkEntry = find(network.networkEntries, (aEntry) => aEntry.origin === currentLocation)!;
+		const direction = aNetwork.directions[directionCounter % aNetwork.directions.length];
+		const networkEntry = find(aNetwork.networkEntries, (aEntry) => aEntry.origin === currentLocation)!;
 		currentLocation = direction === Directions.LEFT ? networkEntry?.left : networkEntry?.right;
 
 		directionCounter++;
 	}
 
 	console.log('Part 1: ', directionCounter);
+}
+
+function solvePart2(aNetwork: Network) {
+	let currentLocations = aNetwork.networkEntries.filter((entry) => endsWith(entry.origin, 'A')).map((aEntry) => aEntry.origin);
+
+	const numbersOfDirections = currentLocations.map((aCurrentLocation) => {
+		let directionCounter = 0;
+
+		while (!endsWith(aCurrentLocation, 'Z')) {
+			const direction = aNetwork.directions[directionCounter % aNetwork.directions.length];
+			const networkEntry = find(aNetwork.networkEntries, (aEntry) => aEntry.origin === aCurrentLocation)!;
+			aCurrentLocation = direction === Directions.LEFT ? networkEntry?.left : networkEntry?.right;
+
+			directionCounter++;
+		}
+
+		return directionCounter;
+	});
+
+	const greatestCommonDivisor: any = (a: number, b: number) => (b === 0 ? a : greatestCommonDivisor(b, a % b));
+
+	const leastCommonMultiple = (aNumbersOfDirections: number[]) => {
+		const lcmTwoNumbers = (a: number, b: number) => (a * b) / greatestCommonDivisor(a, b);
+
+		return aNumbersOfDirections.reduce((acc, num) => lcmTwoNumbers(acc, num), 1);
+	};
+
+	console.log('Part 2: ', leastCommonMultiple(numbersOfDirections));
+}
+
+export function solveDay8() {
+	let network = parseData(DAY8_DATA);
+
+	// solvePart1(network);
+	solvePart2(network);
 }
